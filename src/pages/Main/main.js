afe186f3a6336f62da0278a07466f20978413c22
王晓 <wangxiao@wangxiaodeMacBook-Pro.local>
2017-09-07 15:49:34 +0800

    xt

---
+++ src/pages/Main/main.js
@@ -0,0 +1,17 @@
+
+import React,{ Component } from 'react';
+export default class Main extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			data: ''
+		}
+	}
+	render(){
+		return (
+			<div>
+				主页面
+			</div>
+		)
+	}
+}
---
+++ src/router/router.js
@@ -0,0 +1,24 @@
+//router.js
+import React,{ Component } from 'react';
+import {
+	BrowserRouter as Router,
+	Link,
+	Route
+} from 'react-router-dom';
+
+
+
+export default class RouterMap extends Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return (
+			<Router>
+				<div>
+					
+				</div>
+			</Router>
+		)
+	}
+}
