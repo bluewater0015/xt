86209de2dc73e99fc674588e563ac125a26183a5
王晓 <wangxiao@wangxiaodeMacBook-Pro.local>
2017-09-07 19:28:13 +0800

    环境搭建

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 
 
-
 import React,{ Component } from 'react';
 import { render } from 'react-dom';
 import './css/common.css'
@@ -13,11 +12,11 @@ import thunkMiddleware from 'redux-thunk';
 const store = createStore(rootReducers,
     applyMiddleware(thunkMiddleware)
 );
-import Main from './pages/Main/main';
+import Main from './pages/Main';
 class App extends Component{
     render(){
         return (
-            <div className="fx1">
+            <div>
                 <Provider store={store}>
                     <Main />
                 </Provider>
--- src/pages/income_Detail/income_Detail.js
+++ src/pages/income_Detail/income_Detail.js
@@ -1,16 +1,14 @@
-
+//income_Detail.js
 import React,{ Component } from 'react';
-export default class Main extends Component{
+
+export default class Income extends Component{
 	constructor(props){
 		super(props);
-		this.state = {
-			data: ''
-		}
 	}
 	render(){
 		return (
 			<div>
-				主页面
+				机器收入详情页	
 			</div>
 		)
 	}
